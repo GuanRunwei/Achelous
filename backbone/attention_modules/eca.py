57599eaf53c3431b5a3daa6f7972e48bc2a2b61b
Runwei Guan <31555628+guanrunwei@users.noreply.github.com>
2023-03-17 15:39:35 +0800

    no

---
+++ backbone/attention_modules/eca.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class eca_block(nn.Module):
+    def __init__(self, channel, b=1, gamma=2):
+        super(eca_block, self).__init__()
+        kernel_size = int(abs((math.log(channel, 2) + b) / gamma))
+        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        output = self.avg_pool(x)
+        # print("average pool shape:", output.shape)
+        output = self.conv(output.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        output = self.sigmoid(output)
+
+        return x * output.expand_as(x)
+
+
